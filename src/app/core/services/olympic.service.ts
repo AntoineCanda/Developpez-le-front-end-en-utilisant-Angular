d1610ffd00e50aea01d9a0bb025787bd90484c04
Antoine Canda <candaantoine@gmail.com>
2024-04-08 18:23:20 +0200

    Merge branch 'feat_oc35_service'

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,31 +1,38 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { IOlympic } from '../models/Olympic';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new BehaviorSubject<IOlympic[]>([]);
 
   constructor(private http: HttpClient) {}
 
-  loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
-      tap((value) => this.olympics$.next(value)),
-      catchError((error, caught) => {
-        // TODO: improve error handling
-        console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
-        return caught;
+  loadInitialData():  Observable<void | IOlympic[]> {
+    return this.http.get<IOlympic[]>(this.olympicUrl).pipe(
+      tap((olympics) => this.olympics$.next(olympics)),
+      catchError((error: HttpErrorResponse) => {
+        // Gestion des erreurs
+        console.error('Error fetching Olympics data:', error.message);
+        // Laisser le sujet dans un état "complet" avec une liste vide
+        this.olympics$.next([]);
+        // Retourner une nouvelle observable émettant l'erreur
+        return new Observable<void>((observer) => {
+          observer.error(error);
+        });
       })
     );
   }
 
-  getOlympics() {
+  // Méthode pour récupérer les données des Jeux Olympiques
+  getOlympics(): Observable<IOlympic[]> {
     return this.olympics$.asObservable();
   }
 }
+
+
